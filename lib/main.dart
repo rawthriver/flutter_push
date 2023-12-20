2bd810ddb78dbc20f73dad56aeec183cf98bb8cf
rawthriver <rawthriver@yandex.ru>
2023-12-20 10:15:39 +0300

    'ready'

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,119 +10,127 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  GameState _gameState = GameState.readyToStart;
+  String millisecondsText = '';
+  Timer? _waitingTimer;
+  Timer? _stoppableTimer;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  String _getButtonText() => switch (_gameState) {
+    GameState.readyToStart => 'START',
+    GameState.waiting => 'WAIT',
+    GameState.canBeStopped => 'STOP'
+  };
+
+  final _buttonColor = <GameState, int>{
+    GameState.readyToStart: 0xFF40CA88,
+    GameState.waiting: 0xFFE0982D,
+    GameState.canBeStopped: 0xFFE02D47
+  };
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      backgroundColor: const Color(0xFF282E3D),
+      body: Stack(
+        children: [
+          const Align(
+            alignment: Alignment(0, -0.8),
+            child: Text(
+              'Test your \n reaction speed',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white, fontSize: 38, fontWeight: FontWeight.w900),
+            )
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: ColoredBox(
+              color: const Color(0xFF6D6D6D),
+              child: SizedBox(
+                width: 300,
+                height: 108,
+                child: Center(
+                  child: Text(
+                    millisecondsText,
+                    style: const TextStyle(color: Colors.white, fontSize: 36, fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ),
+            )
+          ),
+          Align(
+            alignment: const Alignment(0, 0.8),
+            child: GestureDetector(
+              onTap: () => setState(() {
+                switch (_gameState) {
+                  case GameState.readyToStart:
+                    _gameState = GameState.waiting;
+                    millisecondsText = '...';
+                    _startWaitingTimer();
+                  case GameState.waiting:
+                    //actually do nothing
+                    break;
+                  case GameState.canBeStopped:
+                    _gameState = GameState.readyToStart;
+                    _stoppableTimer?.cancel();
+                }
+              }),
+              child: ColoredBox(
+                color: Color(_buttonColor[_gameState]!),
+                child: SizedBox(
+                  width: 300,
+                  height: 108,
+                  child: Center(
+                    child: Text(
+                      _getButtonText(),
+                      style: const TextStyle(color: Colors.white, fontSize: 38, fontWeight: FontWeight.w900),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ),
+
+        ]
+      )
     );
   }
+
+  @override
+  void dispose() {
+    _waitingTimer?.cancel();
+    _stoppableTimer?.cancel();
+    super.dispose();
+  }
+
+  void _startWaitingTimer() {
+    _waitingTimer = Timer(Duration(seconds: Random().nextInt(4) + 1), () {
+      setState(() => _gameState = GameState.canBeStopped);
+      _startStoppableTimer();
+    });
+  }
+  
+  void _startStoppableTimer() {
+    _stoppableTimer = Timer.periodic(const Duration(milliseconds: 16), (timer) {
+      setState(() => millisecondsText = '${(timer.tick * 16)} ms');
+    });
+  }
+  
 }
+
+enum GameState { readyToStart, waiting, canBeStopped }
